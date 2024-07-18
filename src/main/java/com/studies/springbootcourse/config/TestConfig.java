45b56334b8b7b306e2b3bcc9e96abdd9abcdba1f
Alexsander de Morais <alexsanderm@kyros.com.br>
2024-07-17 22:04:46 -0300

    Add: payment and one to one association

--- src/main/java/com/studies/springbootcourse/config/TestConfig.java
+++ src/main/java/com/studies/springbootcourse/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.studies.springbootcourse.entities.Category;
 import com.studies.springbootcourse.entities.Order;
 import com.studies.springbootcourse.entities.OrderItem;
+import com.studies.springbootcourse.entities.Payment;
 import com.studies.springbootcourse.entities.Product;
 import com.studies.springbootcourse.entities.User;
 import com.studies.springbootcourse.entities.enums.OrderStatus;
@@ -92,5 +93,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-07-22T15:24:22Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
